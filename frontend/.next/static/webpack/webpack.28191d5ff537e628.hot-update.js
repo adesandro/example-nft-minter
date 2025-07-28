e543d9db5b29a0dc292747ddec758f5c3010d1d9
Joseph Zhang <joseph@nexus.xyz>
2025-07-28 14:46:30 -0700

    fixes for metadata

--- frontend/.next/static/webpack/webpack.28191d5ff537e628.hot-update.js
+++ frontend/.next/static/webpack/webpack.28191d5ff537e628.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "459a07993fdad80e"; }
+/******/ 	__webpack_require__.h = function() { return "56396da13dab50d4"; }
 /******/ }();
 /******/ 
 /******/ }
